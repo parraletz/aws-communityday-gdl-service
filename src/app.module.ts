6fd61a462d92f43d93655b87693f3e6517af2c65
Alex Parra <parraletz@gmail.com>
2024-10-04 22:46:39 -0600

    feat: Added devpod environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,8 +34,8 @@ import { UsersModule } from './users/users.module'
         PORT: Joi.number().default(3000),
         API_KEY: Joi.string().required(),
         DATABASE_URL: Joi.string().required(),
-        REDIS_HOST: Joi.string(),
-        REDIS_PORT: Joi.number(),
+        REDIS_HOST: Joi.string().default('redis'),
+        REDIS_PORT: Joi.number().default(6379),
       }),
       validationOptions: {
         allowUnknown: true,
